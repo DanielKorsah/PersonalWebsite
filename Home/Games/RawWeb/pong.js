663e590f3e135978fc3042bc5f85815bcea791cf
Daniel Korsah <dkorsah15@aol.co.uk>
2019-06-19 13:57:10 +0100

    Start on Pong page

---
+++ Home/Games/RawWeb/pong.js
@@ -0,0 +1,13 @@
+(function () {
+
+    var gamesize = {
+        width = 1000,
+        height = 500
+    };
+
+    // 2D canvas
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+
+
+})();
